a499fb1737a8f2c4ea6998d6fa0449211bf32151
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-05-28 08:57:53 +0300

    lessin_5

---
+++ src/layouts/PageLayout/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import Footer from '../../shared/Footer';
+import Header from '../../shared/Header';
+import style from './PageLayout.module.scss';
+
+interface IPageLayout {
+  children: React.ReactNode;
+}
+
+const PageLayout: FC<IPageLayout> = ({ children }) => {
+  return (
+    <div className={style.page_wrapper}>
+      <div className={style.section_wrapper}>
+        <Header />
+        <div className={style.content_wrapper}>{children}</div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default PageLayout;
